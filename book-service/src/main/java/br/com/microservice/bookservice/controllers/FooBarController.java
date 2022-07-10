7562216a9293589bc7b22aed0b135cf6ca1d83a7
Jean Fernandine <jfernandine@gmail.com>
2022-07-09 21:14:03 -0300

    circuitBreaker and resilience4j

--- book-service/src/main/java/br/com/microservice/bookservice/controllers/FooBarController.java
+++ book-service/src/main/java/br/com/microservice/bookservice/controllers/FooBarController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
